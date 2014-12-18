e54d3e2c4837af0e494933951ebc05c17a0b89c7
Francis Besset <francis.besset@gmail.com>
2014-12-18 09:16:06 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM debian:wheezy
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update && apt-get upgrade -y
+
+RUN apt-get install -y --no-install-recommends tinyproxy
+
+RUN mkdir /var/run/tinyproxy && \
+    chown nobody /var/run/tinyproxy
+
+RUN touch /var/log/tinyproxy/tinyproxy.log && \
+    chown nobody /var/log/tinyproxy/tinyproxy.log
+
+ADD start.sh /
+RUN chown nobody /start.sh && chmod +x /start.sh
+
+EXPOSE 8888
+
+ENTRYPOINT [ "/start.sh" ]
---
+++ start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo
+    echo ' Usage:'
+    echo '      docker run -d --name="tinyproxy" [-e "LOG_LEVEL=<LogLevel>"] -p <HostPort>:8888 tinyproxy <ACL>'
+    echo
+    echo '          - Set <LogLevel>. Can be set with "Info" (default value), "Connect", "Notice", "Warning", "Error" or "Critical"'
+    echo '          - Set <HostPort> to the port you wish the proxy to be accessible from.'
+    echo '          - Set <ACL> to "ANY" to allow unrestricted proxy access, or one or more space seperated IP/CIDR addresses for tighter security.'
+    echo
+    echo '      Examples:'
+    echo '          docker run -d --name="tinyproxy" -p 1111:8888 tinyproxy ANY'
+    echo '          docker run -d --name="tinyproxy" -p 2222:8888 tinyproxy 123.45.67.89'
+    echo '          docker run -d --name="tinyproxy" -p 3333:8888 tinyproxy 123.45.67.89 192.168.0.0/16'
+    echo '          docker run -d --name="tinyproxy" -p 4444:8888 -e "LOG_LEVEL=Notice" tinyproxy 123.45.67.89'
+    echo '          docker run -d --name="tinyproxy" -p 5555:8888 -e "LOG_LEVEL=Warning" tinyproxy 123.45.67.89 192.168.0.0/16'
+    echo
+
+    exit 1
+fi
+
+# Allow
+for allow in $@; do
+    echo "Allow ${allow}" >> /etc/tinyproxy.conf
+done
+
+# LogLevel
+if [ -n "${LOG_LEVEL}" ]; then
+    sed -i "s/LogLevel Info/LogLevel ${LOG_LEVEL}/" /etc/tinyproxy.conf
+fi
+
+tinyproxy -c /etc/tinyproxy.conf
+tail -f /var/log/tinyproxy/tinyproxy.log
