e1dcf33b09145a388307cc1c3f62bcf8a19ebd76
Francis Besset <francis.besset@gmail.com>
2015-09-15 12:43:17 +0200

    Added CONNECT_PORT variable environment

--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@ if [ "$#" -lt 1 ]; then
     echo '          docker run -d --name="tinyproxy" -p 3333:8888 tinyproxy 123.45.67.89 192.168.0.0/16'
     echo '          docker run -d --name="tinyproxy" -p 4444:8888 -e "LOG_LEVEL=Notice" tinyproxy 123.45.67.89'
     echo '          docker run -d --name="tinyproxy" -p 5555:8888 -e "LOG_LEVEL=Warning" tinyproxy 123.45.67.89 192.168.0.0/16'
+    echo '          docker run -d --name="tinyproxy" -p 6666:8888 -e "CONNECT_PORT=all" tinyproxy 123.45.67.89 192.168.0.0/16'
     echo
 
     exit 1
@@ -28,6 +29,12 @@ done
 # LogLevel
 if [ -n "${LOG_LEVEL}" ]; then
     sed -i "s/LogLevel Info/LogLevel ${LOG_LEVEL}/" /etc/tinyproxy.conf
+fi
+
+if [ -n "${CONNECT_PORT}" ]; then
+    if [ "all" == "${CONNECT_PORT}" ]; then
+        sed -i "s/ConnectPort/#ConnectPort/" /etc/tinyproxy.conf
+    fi
 fi
 
 tinyproxy -c /etc/tinyproxy.conf
